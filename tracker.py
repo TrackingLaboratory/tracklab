98afe9ba4231a3b0ba35766ec7d400bf37aeeb67
Victor Joos <victor.joos@uclouvain.be>
2022-11-02 11:54:08 +0100

    Flat Detection dataclass

--- tracker.py
+++ tracker.py
@@ -1,46 +1,66 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import Enum
+from typing import Optional
 import numpy as np
-
-
-@dataclass
-class Metadata:
-    filename: str
-    height: int
-    width: int
-    video_id: str # or int ?
-    frame: int
-    frames: int
-
-@dataclass
-class Bbox:
-    xc: float
-    yc: float
-    w: float
-    h: float
-    conf: float
-
-@dataclass
-class Keypoint:
-    x: float
-    y: float
-    conf: float
+import pandas as pd
 
 class Source(Enum):
     GT = 1
     DET = 2
     TRACK = 3
 
-
 @dataclass
 class Detection:
-    metadata: Metadata
-    bbox: Bbox
-    keypoint: Keypoint
-    source: Source
-    id_features: np.ndarray # store as numpy or as torch ?
+    # Metadata
+    filename: str = ""
+    height: int = 0
+    width: int = 0
+    video_id: str = "" # or int ?
+    frame: int = 0
+    frames: int = 0
+    source: Optional[Source] = None
+
+    # Bbox
+    bb_xc: float = np.nan
+    bb_yc: float = np.nan
+    bb_w: float = np.nan
+    bb_h: float = np.nan
+    bb_conf: float = np.nan
+
+    # Keypoint
+    keypoint : Optional[np.ndarray] = None
+
+    # ReID
+    id_features: Optional[np.ndarray] = None
 
 
 @dataclass
 class Tracker:
-    detections: list[Detection] = []
+    detections: list[Detection] = field(default_factory=list)
+    _dataframe: Optional[pd.DataFrame] = None
+    _dirty: bool = True
+
+    @property
+    def dataframe(self):
+        if True: # self._dirty:
+            self._dataframe = pd.DataFrame(self.detections)
+            self._dirty = False
+
+        return self._dataframe
+
+    def add_detection(self, detection):
+        self.detections.append(detection)
+        self._dirty = True
+
+
+
+def main():
+    tracker = Tracker()
+    
+    for _ in range(100_000):
+        tracker.add_detection(Detection())
+    
+    return tracker
+    
+if __name__ == "__main__":
+    print(main().dataframe.head())
