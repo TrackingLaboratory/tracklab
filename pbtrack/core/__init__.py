e9f13b4abcee6941d94da3771643d4ed61e6712b
Victor Joos <victor.joos@uclouvain.be>
2022-12-08 14:03:40 +0100

    Adds datapipe for engine processing

---
+++ pbtrack/core/__init__.py
@@ -0,0 +1,5 @@
+from .detector import Detector
+from .reidentifier import ReIdentifier
+from .tracker import Tracker
+from .tracking_engine import OnlineTrackingEngine
+from .datapipe import EngineDatapipe
