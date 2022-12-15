7e9562d9bd1fefb551c603d8e09c00ff7be2a6bf
bstandaert <baptiste.standaert@uclouvain.be>
2022-12-15 15:10:48 +0100

    modifications for new openpifpaf

--- wrappers/detect/openpifpaf_api.py
+++ wrappers/detect/openpifpaf_api.py
@@ -21,6 +21,7 @@ class OpenPifPaf(Detector):
         if cfg.checkpoint:
             self.predictor = openpifpaf.Predictor(cfg.checkpoint)
             self.predictor.model.to(device)
+            self.predictor.batch_size = cfg.batch_size
 
     def preprocess(self, metadata):
         return str(metadata.file_path)
@@ -40,15 +41,18 @@ class OpenPifPaf(Detector):
                         bbox_ltwh=kp_to_bbox_w_threshold(
                             prediction.data, vis_threshold=0.05
                         ),
-                    )  # type: ignore
+                    )
                 )
                 self.id += 1
         return detections
 
     def train(self):
         saved_argv = sys.argv
-        sys.argv += [f"--{str(k)}={str(v)}" for k, v in self.cfg.train.items()]
+        sys.argv += [
+            f"--{str(k)}={str(v)}" for k, v in self.cfg.train.items()
+        ]  # FIXME add multiple args
         self.cfg.checkpoint = openpifpaf.train.main()
         sys.argv = saved_argv
         self.predictor = openpifpaf.Predictor(self.cfg.checkpoint)
         self.predictor.model.to(self.device)
+        self.predictor.batch_size = self.cfg.batch_size
