5a84a0bf3e718b13ef01a16aa526e7e1aafa79d4
bstandaert <baptiste.standaert@uclouvain.be>
2023-01-05 10:50:43 +0100

    add of skeleton vis

--- pbtrack/visualization/vis_engine.py
+++ pbtrack/visualization/vis_engine.py
@@ -6,13 +6,50 @@ from pathlib import Path
 from pbtrack.utils.coordinates import clip_bbox_ltrb_to_img_dim
 from pbtrack.utils.images import cv2_load_image, overlay_heatmap
 
-import matplotlib.cm as cm
+cmap = [
+    [230, 25, 75],
+    [60, 180, 75],
+    [255, 225, 25],
+    [0, 130, 200],
+    [245, 130, 48],
+    [145, 30, 180],
+    [70, 240, 240],
+    [240, 50, 230],
+    [210, 245, 60],
+    [250, 190, 212],
+    [0, 128, 128],
+    [220, 190, 255],
+    [170, 110, 40],
+    [255, 250, 200],
+    [128, 0, 0],
+    [170, 255, 195],
+    [128, 128, 0],
+    [255, 215, 180],
+    [0, 0, 128],
+    [128, 128, 128],
+]
 
-cmap = (
-    (255 * cm.colors.to_rgba_array(cm.get_cmap("tab10")(range(10))))[:, :3]
-    .astype(int)
-    .tolist()
-)
+human_squeleton = [
+    [16, 14],
+    [14, 12],
+    [17, 15],
+    [15, 13],
+    [12, 13],
+    [6, 12],
+    [7, 13],
+    [6, 7],
+    [6, 8],
+    [7, 9],
+    [8, 10],
+    [9, 11],
+    [2, 3],
+    [1, 2],
+    [1, 3],
+    [2, 4],
+    [3, 5],
+    [4, 6],
+    [5, 7],
+]
 
 
 # TODO show person's squeleton
@@ -28,7 +65,7 @@ class VisEngine:
             return
         image_metadatas = tracker_state.gt.image_metadatas[
             tracker_state.gt.image_metadatas.video_id == video_id
-        ]  # FIXME sort?
+        ]
         for i, image_id in enumerate(image_metadatas.id):
             if i > self.cfg.process_n_frames_by_video != -1:
                 break
@@ -53,18 +90,20 @@ class VisEngine:
         patch = cv2_load_image(image_metadata.file_path)
 
         # bboxes
-        if self.cfg.bbox.show_preds:
-            patch = self._plot_bbox(detections, patch, preds=True)
-        if self.cfg.bbox.show_gt:
-            patch = self._plot_bbox(detections_gt, patch, preds=False)
+        if self.cfg.bbox.draw_predictions:
+            patch = self._draw_bbox(detections, patch, is_predictions=True)
+        if self.cfg.bbox.draw_ground_truth:
+            patch = self._draw_bbox(detections_gt, patch, is_predictions=False)
+
         # keypoints
-        if self.cfg.keypoints.show_preds:
-            patch = self._plot_keypoints(detections, patch, preds=True)
-        if self.cfg.keypoints.show_gt:
-            patch = self._plot_keypoints(detections_gt, patch, preds=False)
+        if self.cfg.keypoints.draw_predictions:
+            patch = self._draw_keypoints(detections, patch, is_predictions=True)
+        if self.cfg.keypoints.draw_ground_truth:
+            patch = self._draw_keypoints(detections_gt, patch, is_predictions=False)
+
         # bpbreid
-        if self.cfg.bpbreid.show_heatmaps:
-            patch = self._plot_bpbreid_heatmaps(detections, patch)
+        if self.cfg.bpbreid.draw_heatmaps:
+            patch = self._draw_bpbreid_heatmaps(detections, patch)
 
         patch = self._final_patch(patch)
         if self.cfg.save_images:
@@ -74,18 +113,22 @@ class VisEngine:
         if self.cfg.save_videos:
             self._update_video(patch, video_save_dir, video_id)
 
-    def _plot_bbox(self, detections, patch, preds):
+    def _draw_bbox(self, detections, patch, is_predictions):
         for _, detection in detections.iterrows():
-            if self.cfg.bbox.show_only_tracked and not detection.track_id:
+            if self.cfg.bbox.draw_only_tracked and not detection.track_id:
                 continue
             if not pd.isna(detection.track_id):
                 color = (
                     cmap[detection.track_id % len(cmap)]
-                    if preds
+                    if is_predictions
                     else self.cfg.bbox.color_gt
                 )
             else:
-                color = self.cfg.bbox.color_preds if preds else self.cfg.bbox.color_gt
+                color = (
+                    self.cfg.bbox.color_preds
+                    if is_predictions
+                    else self.cfg.bbox.color_gt
+                )
             bbox = detection.bbox_ltrb
             bbox = clip_bbox_ltrb_to_img_dim(
                 bbox, patch.shape[1], patch.shape[0]
@@ -98,7 +141,7 @@ class VisEngine:
                 thickness=self.cfg.bbox.thickness,
                 lineType=cv2.LINE_AA,
             )
-            if preds and self.cfg.bbox.print_id:
+            if is_predictions and self.cfg.bbox.print_id:
                 cv2.putText(
                     patch,
                     f"ID: {detection.track_id}",
@@ -109,7 +152,7 @@ class VisEngine:
                     thickness=self.cfg.bbox.font_thickness,
                     lineType=cv2.LINE_AA,
                 )
-            if preds and self.cfg.bbox.print_confidence:
+            if is_predictions and self.cfg.bbox.print_confidence:
                 cv2.putText(
                     patch,
                     f"Conf: {np.mean(detection.keypoints_xyc[:,2]):.2}",
@@ -122,10 +165,15 @@ class VisEngine:
                 )
         return patch
 
-    def _plot_keypoints(self, detections, patch, preds):
-        color = self.cfg.keypoints.color_preds if preds else self.cfg.keypoints.color_gt
+    def _draw_keypoints(self, detections, patch, is_predictions):
+        color = (
+            self.cfg.keypoints.color_preds
+            if is_predictions
+            else self.cfg.keypoints.color_gt
+        )
         all_keypoints = detections.keypoints_xyc
         for keypoints in all_keypoints:
+            # draw keypoints
             for kp in keypoints:
                 cv2.circle(
                     patch,
@@ -135,7 +183,7 @@ class VisEngine:
                     thickness=self.cfg.keypoints.thickness,
                     lineType=cv2.LINE_AA,
                 )
-                if preds and self.cfg.keypoints.print_confidence:
+                if is_predictions and self.cfg.keypoints.print_confidence:
                     cv2.putText(
                         patch,
                         f"{kp[2]:.2}",
@@ -146,10 +194,24 @@ class VisEngine:
                         fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
                         lineType=cv2.LINE_AA,
                     )
+            # draw squeleton
+            if self.cfg.keypoints.draw_squeleton:
+                for link in human_squeleton:
+                    kp1 = keypoints[link[0] - 1]
+                    kp2 = keypoints[link[1] - 1]
+                    if kp1[2] > 0 and kp2[2] > 0:
+                        cv2.line(
+                            patch,
+                            (int(kp1[0]), int(kp1[1])),
+                            (int(kp2[0]), int(kp2[1])),
+                            color=color,
+                            thickness=self.cfg.keypoints.squeleton_thickness,
+                            lineType=cv2.LINE_AA,
+                        )
         return patch
 
-    def _plot_bpbreid_heatmaps(self, detections, patch):
-        for i, detection in detections.iterrows():
+    def _draw_bpbreid_heatmaps(self, detections, patch):
+        for _, detection in detections.iterrows():
             ltrb = detection.bbox_ltrb
             l, t, r, b = clip_bbox_ltrb_to_img_dim(
                 ltrb, patch.shape[1], patch.shape[0]
