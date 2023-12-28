9e5b1e5b34d255954418f7291903d6f0d2506d34
Vladimir <somers.vladimir@gmail.com>
2023-12-28 12:28:59 +0100

    Temporary fix for MMDet version compatibility issue

--- pbtrack/wrappers/detect_multiple/__init__.py
+++ pbtrack/wrappers/detect_multiple/__init__.py
@@ -1,4 +1,4 @@
-from .mmdetection_api import MMDetection
+# from .mmdetection_api import MMDetection
 from .bottomup_mmpose_api import BottomUpMMPose
 from .openpifpaf_api import OpenPifPaf
 from .yolov8_api import YOLOv8
