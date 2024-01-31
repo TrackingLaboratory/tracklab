849bb21ce67ea445c7bcc791f0b736fa9bbd71b9
Vladimir <somers.vladimir@gmail.com>
2024-01-31 11:22:50 +0100

    Fix video_loop wrong return when empty pipeline

--- tracklab/engine/offline.py
+++ tracklab/engine/offline.py
@@ -13,23 +13,22 @@ class OfflineTrackingEngine(TrackingEngine):
                 model.reset()
 
         detections, image_pred = tracker_state.load()
-        if len(self.module_names) == 0:
-            return detections
-        image_filepaths = {idx: fn for idx, fn in image_pred["file_path"].items()}
-        model_names = self.module_names
-        for model_name in model_names:
-            if self.models[model_name].level == "video":
-                detections = self.models[model_name].process(detections, image_pred)
-                continue
-            self.datapipes[model_name].update(image_filepaths, image_pred, detections)
-            self.callback(
-                "on_module_start",
-                task=model_name,
-                dataloader=self.dataloaders[model_name],
-            )
-            for batch in self.dataloaders[model_name]:
-                detections, image_pred = self.default_step(batch, model_name, detections, image_pred)
-            self.callback("on_module_end", task=model_name, detections=detections)
-            if detections.empty:
-                return detections, image_pred
+        if len(self.module_names) > 0:
+            image_filepaths = {idx: fn for idx, fn in image_pred["file_path"].items()}
+            model_names = self.module_names
+            for model_name in model_names:
+                if self.models[model_name].level == "video":
+                    detections = self.models[model_name].process(detections, image_pred)
+                    continue
+                self.datapipes[model_name].update(image_filepaths, image_pred, detections)
+                self.callback(
+                    "on_module_start",
+                    task=model_name,
+                    dataloader=self.dataloaders[model_name],
+                )
+                for batch in self.dataloaders[model_name]:
+                    detections, image_pred = self.default_step(batch, model_name, detections, image_pred)
+                self.callback("on_module_end", task=model_name, detections=detections)
+                if detections.empty:
+                    return detections, image_pred
         return detections, image_pred
