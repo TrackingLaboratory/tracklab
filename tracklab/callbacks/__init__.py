b9912270c3058f36f21658cf17e15b2ce39606c0
Victor Joos <victor.joos@uclouvain.be>
2024-02-29 13:17:13 +0100

    Add timer callback

--- tracklab/callbacks/__init__.py
+++ tracklab/callbacks/__init__.py
@@ -1,3 +1,4 @@
 from .callback import Callback
 from .progress import Progressbar, RichProgressbar
 from .handle_regions import IgnoredRegions
+from .timer import Timer
---
+++ tracklab/callbacks/timer.py
@@ -0,0 +1,53 @@
+import logging
+import time
+from datetime import timedelta
+from typing import Optional, Any
+
+import pandas as pd
+from torch.utils.data import DataLoader
+
+from tracklab.callbacks import Callback
+from tracklab.engine import TrackingEngine
+
+log = logging.getLogger(__name__)
+
+
+class Timer(Callback):
+    def __init__(self, **kwargs):
+        self.start_times = {}
+
+    def on_dataset_track_start(self, engine: TrackingEngine):
+        self.start_times["dataset"] = time.perf_counter()
+
+    def on_dataset_track_end(self, engine: TrackingEngine):
+        time_delta = timedelta(seconds=time.perf_counter() - self.start_times["dataset"])
+        log.info(f"Dataset time : {time_delta}")
+
+    def on_video_loop_start(
+        self, engine: TrackingEngine, video_metadata: pd.Series, video_idx: int, index: int
+    ):
+        self.start_times["video"] = time.perf_counter()
+
+    def on_video_loop_end(
+        self,
+        engine: TrackingEngine,
+        video_metadata: pd.Series,
+        video_idx: int,
+        detections: pd.DataFrame,
+        image_pred: pd.DataFrame,
+    ):
+        end = time.perf_counter()
+        time_delta = timedelta(seconds=end - self.start_times["video"])
+        frames = len(image_pred)
+        fps = frames / (end - self.start_times["video"])
+        log.info(f"Video time : {time_delta}, FPS : {fps}")
+
+    def on_module_start(self, engine: TrackingEngine, task: str, dataloader: DataLoader):
+        self.start_times[task] = time.perf_counter()
+
+    def on_module_end(self, engine: TrackingEngine, task: str, detections: pd.DataFrame):
+        end = time.perf_counter()
+        time_delta = timedelta(seconds=end - self.start_times[task])
+        frames = len(detections.image_id.unique())
+        fps = frames / (end-self.start_times[task])
+        log.info(f"Module {task} time : {time_delta}, FPS : {fps}")
