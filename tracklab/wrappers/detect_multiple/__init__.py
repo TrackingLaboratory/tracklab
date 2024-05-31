21b60261425e8958d94c25114396c74c543b1a5b
Victor Joos <victor.joos@uclouvain.be>
2024-05-31 11:07:53 +0200

    Remove openpifpaf dependency (to be reverted soon)

--- tracklab/wrappers/detect_multiple/__init__.py
+++ tracklab/wrappers/detect_multiple/__init__.py
@@ -1,5 +1,5 @@
 # from .mmdetection_api import MMDetection
 # from .bottomup_mmpose_api import BottomUpMMPose
-from .openpifpaf_api import OpenPifPaf
+# from .openpifpaf_api import OpenPifPaf  # FIXME : removed until dependency problem is fixed
 from .yolov8_api import YOLOv8
 from .yolov8_pose_api import YOLOv8Pose
