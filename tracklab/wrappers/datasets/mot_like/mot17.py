527789b47d3c1a9b697dfcfad9ebb4c8f7b4ade3
bstandaert <baptiste.standaert@uclouvain.be>
2024-06-21 16:42:53 +0200

    Bootstrap load from public detections

--- tracklab/wrappers/datasets/mot_like/mot17.py
+++ tracklab/wrappers/datasets/mot_like/mot17.py
@@ -22,6 +22,6 @@ categories_list = [
 
 class MOT17(MOT):
     def __init__(self, dataset_path: str, nvid: int = -1,
-                 vids_dict: list = None, *args, **kwargs):
+                 vids_dict: list = None, public_dets_subpath : str = None, *args, **kwargs):
         log.info(f"Loading MOT17 dataset from {dataset_path}.")
-        super().__init__(dataset_path, categories_list, nvid, vids_dict, *args, **kwargs)
+        super().__init__(dataset_path, categories_list, nvid, vids_dict, public_dets_subpath, *args, **kwargs)
