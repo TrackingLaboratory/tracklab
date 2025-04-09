8c9d7f44ac98f9264d862f67d44e78fccb30d63d
Victor Joos <victor.joos@uclouvain.be>
2025-04-09 15:05:16 +0200

    Fix for py>=3.12

--- hydra_plugins/tracklab_searchpath_plugin/tracklab_searchpath_plugin.py
+++ hydra_plugins/tracklab_searchpath_plugin/tracklab_searchpath_plugin.py
@@ -1,6 +1,8 @@
+import sys
+
 from hydra.core.config_search_path import ConfigSearchPath
 from hydra.plugins.search_path_plugin import SearchPathPlugin
-from importlib.metadata import entry_points
+from importlib import metadata
 import logging
 
 log = logging.getLogger(__name__)
@@ -9,12 +11,24 @@ log = logging.getLogger(__name__)
 class PbTrackSearchPathPlugin(SearchPathPlugin):
     def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
         # Appends the search path for tracklab plugins to the end of the search path
-        groups = entry_points()
-        pbtrack_plugins = groups.get("tracklab_plugin", [])
-        for pbtrack_plugin in pbtrack_plugins:
-            m = pbtrack_plugin.dist
-            module = pbtrack_plugin.load()
+        tracklab_plugins = entry_points(group="tracklab_plugin")
+        for tracklab_plugin in tracklab_plugins:
+            m = tracklab_plugin.dist
+            module = tracklab_plugin.load()
             if hasattr(module, "config_package"):
                 search_path.append(provider="tracklab", path=module.config_package)
             else:
                 log.warning(f"{module} doesn't provide a config path")
+
+
+def entry_points(*, group: str) -> "metadata.EntryPoints":  # type: ignore[name-defined]
+    """entry_points function that is compatible with Python 3.7+."""
+    if sys.version_info >= (3, 10):
+        return metadata.entry_points(group=group)
+
+    epg = metadata.entry_points()
+
+    if sys.version_info < (3, 8) and hasattr(epg, "select"):
+        return epg.select(group=group)
+
+    return epg.get(group, [])
