fad657c1ab6bb08876d92dbac1b66d9082fe741b
bstandaert <baptiste.standaert@uclouvain.be>
2025-05-02 10:25:27 +0200

    Renaming to more explicit of the frame counter

--- tracklab/visualization/image.py
+++ tracklab/visualization/image.py
@@ -1,10 +1,7 @@
 from .visualizer import ImageVisualizer
-from tracklab.utils.cv2 import print_count_frame
+from tracklab.utils.cv2 import print_count_frame, draw_ignore_region
 
-class PrintCountVisualizer(ImageVisualizer):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
+class FrameCountVisualizer(ImageVisualizer):
     def draw_frame(self, image, detections_pred, detections_gt, image_pred, image_gt):
         print_count_frame(image, image_gt.frame, nframes=image_gt.nframes)
 
