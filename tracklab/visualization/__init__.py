9e0ab7706badd29481269de9e3a14af5c4953f4b
bstandaert <baptiste.standaert@uclouvain.be>
2025-05-02 10:25:00 +0200

    Add of IgnoreRegionsVisualizer as callback

--- tracklab/visualization/__init__.py
+++ tracklab/visualization/__init__.py
@@ -1,7 +1,7 @@
-from .visualizer import Visualizer, ImageVisualizer, DetectionVisualizer
+from .visualizer import Visualizer, ImageVisualizer, DetectionVisualizer, get_fixed_colors
 from .visualization_engine import VisualizationEngine
 from .detection import (DefaultDetectionVisualizer, FullDetectionVisualizer, DebugDetectionVisualizer,
                         EllipseDetectionVisualizer, SimpleDetectionStatsVisualizer, DetectionStatsVisualizer)
 from .keypoints import DefaultKeypointsVisualizer, FullKeypointsVisualizer
 from .tracking import TrackingLineVisualizer
-from .image import PrintCountVisualizer
+from .image import FrameCountVisualizer, IgnoreRegionsVisualizer
--- tracklab/visualization/image.py
+++ tracklab/visualization/image.py
@@ -7,3 +7,7 @@ class PrintCountVisualizer(ImageVisualizer):
 
     def draw_frame(self, image, detections_pred, detections_gt, image_pred, image_gt):
         print_count_frame(image, image_gt.frame, nframes=image_gt.nframes)
+
+class IgnoreRegionsVisualizer(ImageVisualizer):
+    def draw_frame(self, image, detections_pred, detections_gt, image_pred, image_gt):
+        draw_ignore_region(image, image_pred)
