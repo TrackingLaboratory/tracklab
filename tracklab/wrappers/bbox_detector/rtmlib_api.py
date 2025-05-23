60ada094732ce8f015b3d77195f9ef3e97002a90
bstandaert <baptiste.standaert@uclouvain.be>
2025-05-23 14:14:02 +0200

    Yolox from RTMLib integration

--- tracklab/wrappers/bbox_detector/rtmlib_api.py
+++ tracklab/wrappers/bbox_detector/rtmlib_api.py
@@ -1,45 +1,46 @@
 from typing import Any
 
-import numpy as np
-import pandas as pd
+import accelerate
+import rtmlib
 import cv2
-from rtmlib import YOLOX
+import pandas as pd
+
+from hydra.utils import instantiate
 
 from tracklab.pipeline import ImageLevelModule
+from tracklab.utils.coordinates import ltrb_to_ltwh
 
 
-class YOLOXDetector(ImageLevelModule):
+class RTMLibDetector(ImageLevelModule):
     input_columns = []
-    output_columns = [
-        "image_id", "video_id", "category_id", "bbox_ltwh", "bbox_conf"
-    ]
+    output_columns = ["image_id", "video_id", "category_id", "bbox_ltwh", "bbox_conf"]
 
-    def __init__(self, onnx_model, batch_size, device, input_size=(640, 640)):
-        super().__init__(batch_size=batch_size)
+    def __init__(self, device, model, **kwargs):
+        super().__init__(batch_size=1)
         self.device = device
-        self.onnx_model = onnx_model
-        self.input_size = input_size
-        self.onnx_model = "https://download.openmmlab.com/mmpose/v1/projects/rtmposev1/onnx_sdk/yolox_x_8xb8-300e_humanart-a39d44ed.zip"
-        self.model = YOLOX(self.onnx_model, input_size, device=device)
+        self.model = instantiate(model, device=self.device, backend='onnxruntime')
+        self.id = 0
 
     def preprocess(self, image, detections: pd.DataFrame, metadata: pd.Series) -> Any:
-        padded_img = np.ones(
-            (self.input_size[0], self.input_size[1], 3),
-            dtype=np.uint8) * 114
-        ratio = min(self.input_size[0] / image.shape[0],
-                    self.input_size[1] / image.shape[1])
-
-        resized_img = cv2.resize(
-            image,
-            (int(image.shape[1] * ratio), int(image.shape[0] * ratio)),
-            interpolation=cv2.INTER_LINEAR,
-        ).astype(np.uint8)
-        padded_shape = (int(image.shape[0] * ratio), int(image.shape[1] * ratio))
-        padded_img[:padded_shape[0], :padded_shape[1]] = resized_img
-
-        return image, ratio
+        return {}
 
     def process(self, batch: Any, detections: pd.DataFrame, metadatas: pd.DataFrame):
-        images, ratios = batch
-
-        return self.model(batch)
+        image = cv2.imread(metadatas['file_path'].values[0])  # BGR not RGB !
+        shape = (image.shape[1], image.shape[0])
+        bboxes = self.model(image)
+        detections = []
+        for bbox in bboxes:
+            detections.append(
+                pd.Series(
+                    dict(
+                        image_id=metadatas["id"].values[0],
+                        bbox_ltwh=ltrb_to_ltwh(bbox, shape),
+                        bbox_conf=1.0,
+                        video_id=metadatas["video_id"].values[0],
+                        category_id=1,
+                    ),
+                    name=self.id
+                )
+            )
+            self.id += 1
+        return detections
